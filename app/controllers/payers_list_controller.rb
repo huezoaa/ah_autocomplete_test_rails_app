37e2b05d3b43e1e92e27c0800de69c50e0ec5db0
Angel Huezo <angelohuezo@gmail.com>
2016-04-23 12:10:21 -0400

    Added blank controller for payers list
    Changes to be committed:
    new file:   app/assets/javascripts/payers_list.coffee
    new file:   app/assets/stylesheets/payers_list.scss
    new file:   app/controllers/payers_list_controller.rb
    new file:   app/helpers/payers_list_helper.rb
    new file:   test/controllers/payers_list_controller_test.rb

---
+++ app/controllers/payers_list_controller.rb
@@ -0,0 +1,5 @@
+class PayersListController < ApplicationController
+  #CRUD
+
+
+end
---
+++ test/controllers/payers_list_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PayersListControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
