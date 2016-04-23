d30cb3246307db3f87c4ec159326134992673612
Angel Huezo <angelohuezo@gmail.com>
2016-04-23 13:53:53 -0400

    Created first version of import method for json update file. next: make the file an argument next: do not import if synonym is the same as the payer name.
    Changes to be committed:
    modified:   app/models/payers_list.rb
    new file:   app/views/payers_list/payers_form.html.erb
    modified:   db/schema.rb
    new file:   db/update_files/payers_2016-03-10.json

--- app/models/payers_list.rb
+++ app/models/payers_list.rb
@@ -1,2 +1,31 @@
 class PayersList < ActiveRecord::Base
+
+  # attr_accessible :kipu_insurance_id, :payer_name,
+                  # :payer_synonym, :api_payer_id,
+                  # :provider_id, :type  - need to add to migration
+
+
+  def self.import
+      update_folder = 'db/update_files/'
+      import_file = Rails.root.join(update_folder,'payers_2016-03-10.json')
+      records =  JSON.parse(File.read(import_file))
+
+      # puts records
+
+      fields = {}
+      records.each do |payer|
+        payer['PayerSynonyms'].each do |synonym|
+          fields[:kipu_insurance_id] = payer['Id']
+          fields[:api_payer_id]      = payer['ApiPayerId']
+          fields[:payer_name]        = payer['HealthPlanName']
+          fields[:payer_synonym]     = synonym['Name']
+          # puts fields
+          PayersList.create(fields)
+        end
+      end
+      puts 'Import Complete'
+
+
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318124031) do
+ActiveRecord::Schema.define(version: 20160423160629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20160318124031) do
     t.integer  "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "payers_lists", force: :cascade do |t|
+    t.string   "kipu_insurance_id"
+    t.string   "payer_name"
+    t.string   "payer_synonym"
+    t.string   "api_payer_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "tasks", force: :cascade do |t|
