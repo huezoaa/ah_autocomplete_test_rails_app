93d7350415c4bc9390ada1c42be5dafeb0be3aca
Angel Huezo <angelohuezo@gmail.com>
2016-04-23 17:36:41 -0400

    WIP.  _payers_form gets called by index action but has errors.  followed example in healthmatters. Will try with simple form_for next.
    Changes to be committed:
    modified:   app/controllers/payers_controller.rb
    new file:   app/views/payers/_payers_form.html.erb
    modified:   app/views/payers/index.html.erb
    copied:     app/views/payers/index.html.erb -> app/views/payers/original_index.html.erb

--- app/controllers/payers_controller.rb
+++ app/controllers/payers_controller.rb
@@ -3,8 +3,13 @@ class PayersController < ApplicationController
 
   def index
     @payers = Payer.all
-
   end
+
+  def update
+    payers = Payer.update(params[:payer].keys, params[:payer].values)
+    redirect_to payers_path(form: 'payer')
+  end
+
 
 
 end
