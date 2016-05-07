1f7933a62977236b25dcbc8e681ec1ad9011534c
Angel Huezo <angelohuezo@gmail.com>
2016-05-07 13:07:05 -0400

    Edited seed file for spec test #3 in Thoughtbot/Upcase exercise Changes to be committed: modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,25 +28,43 @@ Task.create!(description: 'Yet another task description',
              deadline: '2014-07-03 01:00:00')
 puts Task.all.map(&:description)
 
-region = Region.create!(name: 'expected')
-other_region = Region.create!(name: 'other')
-puts Region.all.map(&:name)
+## The following 8 lines are only for the 1st spec test of the thoughbot/upcase tutorial
+# region = Region.create!(name: 'expected')
+# other_region = Region.create!(name: 'other')
+# puts Region.all.map(&:name)
 
-## The following 4 lines are only for the 1st spec test of the thoughbot tutorial
 # Location.create!(region: region, name: 'in-expected-region-one')
 # Location.create!(region: region, name: 'in-expected-region-two')
 # Location.create!(region: other_region, name: 'in-other-region')
 # Location.all.map(&:name)
 
-## The following lines are only for the 2nd spec test of the thoughtbot tutorial
-in_region       = Location.create!(region: region, name: "in-expected-region-one")
-in_other_region = Location.create!(region: other_region, name: "in-other-region")
-puts Location.all.map(&:name)
+## The following 8 lines are only for the 2nd spec test of the thoughtbot/upcase tutorial
+# in_region       = Location.create!(region: region, name: "in-expected-region-one")
+# in_other_region = Location.create!(region: other_region, name: "in-other-region")
+# puts Location.all.map(&:name)
 
-Person.create!(location: in_region, name: "in-expected-region-one")
-Person.create!(location: in_region, name: "in-expected-region-two")
-Person.create!(location: in_other_region, name: "in-other-region")
-puts Person.all.map(&:name)
+# Person.create!(location: in_region, name: "in-expected-region-one")
+# Person.create!(location: in_region, name: "in-expected-region-two")
+# Person.create!(location: in_other_region, name: "in-other-region")
+# puts Person.all.map(&:name)
+
+## The following lines are only for the 3rd spec test of the thoughtbot/upcase tutorial
+      region2 = Region.create!(name: "region2")
+      region3 = Region.create!(name: "region3")
+      region1 = Region.create!(name: "region1")
+      location1 = Location.create!(name: "location1", region: region2)
+      location4 = Location.create!(name: "location4", region: region1)
+      location3 = Location.create!(name: "location3", region: region1)
+      location5 = Location.create!(name: "location5", region: region3)
+      location2 = Location.create!(name: "location2", region: region1)
+      Person.create!(name: "person1", location: location5)
+      Person.create!(name: "person5", location: location2)
+      Person.create!(name: "person4", location: location4)
+      Person.create!(name: "person3", location: location1)
+      Person.create!(name: "person2", location: location1)
+      Person.create!(name: "person7", location: location1)
+      Person.create!(name: "person6", location: location3)
+
 
 
 
